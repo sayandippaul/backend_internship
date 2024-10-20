62f808268c4013c76a4424508be46adb52090fa7
sayandippaul <sayandip126@gmail.com>
2024-10-20 23:18:40 +0530

    Update result.js

--- backend/result.js
+++ backend/result.js
@@ -1,16 +1,42 @@
-var votedetails=[];
-votedetails=JSON.parse(localStorage.getItem("votedetails"));
-function showresult(){
-    var obj={
-        name:votedetails[0].name,
-        vote:votedetails[0].vote,
+
+
+var votedetails = [];
+votedetails = JSON.parse(localStorage.getItem("votedetails"));
+
+function showresult() {
+    if (votedetails.length === 0) {
+        document.getElementById("showresult").innerHTML = "No votes available.";
+        return;
     }
-    for(var i=1;i<votedetails.length-1;i++){
-        if(votedetails[i].vote>obj.vote){
-            obj.name=votedetails[i].name;
-            obj.vote=votedetails[i].vote;
+
+    var highest = {
+        name: votedetails[0].name,
+        vote: votedetails[0].vote,
+        id: votedetails[0].id
+    };
+
+    var secondHighest = {
+        name: "",
+        vote: -1,
+        id: -1
+    };
+
+    for (var i = 1; i < votedetails.length; i++) {
+        if (votedetails[i].vote > highest.vote || (votedetails[i].vote === highest.vote && votedetails[i].id > highest.id)) {
+            secondHighest = highest;
+            highest = {
+                name: votedetails[i].name,
+                vote: votedetails[i].vote,
+                id: votedetails[i].id
+            };
+        } else if (votedetails[i].vote > secondHighest.vote || (votedetails[i].vote === secondHighest.vote && votedetails[i].id > secondHighest.id)) {
+            secondHighest = {
+                name: votedetails[i].name,
+                vote: votedetails[i].vote,
+                id: votedetails[i].id
+            };
         }
     }
-    
-    document.getElementById("showresult").innerHTML="The Winner is "+obj.name+" with "+obj.vote+" votes";   
+
+    document.getElementById("showresult").innerHTML = "The Winner is " + highest.name + " with " + highest.vote + " votes. The Runner-up is " + secondHighest.name + " with " + secondHighest.vote + " votes.";
 }
